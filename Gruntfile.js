711891fd8adba9a340a07d562837249b3b68b358
Robert Rullo <rrullo@intuitivecompany.com>
2013-04-08 23:55:10 -0400

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test']
+      }
+    },
+	assemble: {
+            options: {
+                engine: 'handlebars',
+                flatten: true,
+                version: '<%=pkg.version%>'
+            },
+            pages: {
+                files: [
+                    {dest: 'target/docs/', src: ['src/pages/**/*.hbs']
+                    }
+                ]
+            }
+        }
+ });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('assemble');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
